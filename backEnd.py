1765b7231a97c63403c8fe0651a592ccda199688
Abdul Rafay <ybrafay@gmail.com>
2025-10-31 17:16:25 +0530

    good changes finally getting somewhere
    the pages are linked except the FAQ page.
    user login, signup, vendor login,signup working and validating

--- backEnd.py
+++ backEnd.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.5")
 db = client["wholeDB"]
 vendors = db["vendor_credentials"]
+users = db["user_credentials"]
 
 @app.route('/')
 def home():
@@ -13,35 +14,60 @@ def home():
 @app.route('/aboutUs')
 def aboutUs():
     return render_template('aboutUs.html')
-@app.route('/faq')
-def faq():
-    return render_template('FAQ.html')
-@app.route('/userLogin')
-def userLogin():
-    return render_template('userLogin.html')
 
 
-@app.route('/vendorSignUp' , methods=['POST' , 'GET'])
-def vendor_signup():
+@app.route('/userSignUp' , methods=['GET' , 'POST'])
+def userSignUp():
     if request.method == 'GET':
-        return render_template('vendorSignUp.html', message=None)
+        return render_template('userSignUp.html' , message=None)
     if request.method == 'POST':
-        print("FUNCTION CALLED")
         userName = request.form.get('userName')
         password = request.form.get('password')
         confirm = request.form.get('confirm')
-        print("GOT THE DATA")
         if not userName or not password or not confirm:
-            return render_template('vendorSignUp.html', message="⚠️ Please fill in all fields.")
+            return render_template('userSignUp.html', message="Please fill in all fields.")
         if password != confirm:
-            return render_template('vendorSignUp.html', message="⚠️ Passwords do not match.")
+            return render_template('userSignUp.html', message="Passwords do not match.")
+        if users.find_one({"userName": userName}):
+            return render_template('userSignUp.html', message="User '{userName}' already exists!")
+        users.insert_one({"userName": userName, "password": password})
+    return render_template('userSignUp.html',message=None)
+
+@app.route('/userLogin' , methods=['GET' , 'POST'])
+def userLogin():
+    if request.method == 'GET':
+        return render_template('userLogin.html')
+    if request.method == 'POST':
+        userName = request.form.get('userName')
+        password = request.form.get('password')
+        if not userName or not password:
+            return render_template('userLogin.html' , message="please enter both the username and password")
+        user_doc = users.find_one({
+            "userName":userName,
+            "password":password
+        })
+        if user_doc:
+            return render_template('userLogin.html' , message="Login successful")
+        else:
+            return render_template('userLogin.html', message="Login failed, incorrect password or username")
+    return render_template('userLogin.html' , message=None)
+
+@app.route('/vendorSignUp' , methods=['GET', 'POST'])
+def vendor_signup():
+    if request.method == 'GET':
+        return render_template('vendorSignUp.html' , message=None)
+    if request.method == 'POST':
+        userName = request.form.get('userName')
+        password = request.form.get('password')
+        confirm = request.form.get('confirm')
+        if not userName or not password or not confirm:
+            return render_template('vendorSignUp.html', message="Please fill in all fields.")
+        if password != confirm:
+            return render_template('vendorSignUp.html', message="Passwords do not match.")
         if vendors.find_one({"userName": userName}):
-            return render_template('vendorSignUp.html', message=f"⚠️ Vendor '{userName}' already exists!")
-        print("checking...")
-
+            return render_template('vendorSignUp.html', message="Vendor '{userName}' already exists!")
         vendors.insert_one({"userName": userName, "password": password})
-    return render_template('vendorSignUp.html', message=None)
-
+    return render_template('vendorSignUp.html' , message=None)
 
 @app.route('/vendorLogin' , methods=['GET' ,'POST'])
 def vendor_login():
